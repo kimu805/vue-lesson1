1e6f39cfbc2999956d590d04840f1ab75b547094
kimu805 <kimukimuok805@gmail.com>
2025-12-29 15:33:34 +0900

    directive

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,12 @@
 <script setup>
-  import { ref, reactive } from "vue"
-  const title = ref("Vue.js Course")
-  let price = ref(9.99)
-
-  function increment() {
-    price.value += 1
-    instructor.bio = "hi"
-  }
-
-  const info = ref({
-    students: 1000, 
-    rating: 4
-  })
-
-  const instructor = reactive({
-    name: "Kazuto",
-    age: 25,
-    sns: {
-      twitter: "@kazuto",
-      youtube: "@kazuto_channel"
-    },
-    email: ref("kimukimuok805@gmail.com")
-  })
-
-  instructor.bio = "hello"
-  console.log(instructor.name)
-  console.log(info.value.students)
-
-  const courseInfo = {
-    sections: ref(10),
-    language: ref("japanese")
-  }
+import { ref } from 'vue';
+const user = ref({
+  name: "kazuto",
+  age: 25,
+  gender: "man"
+})
 </script>
 
 <template>
-  <h1>Title: {{ title }}</h1>
-  <h2>Price: ${{ price - 1 }}</h2>
-  <button @click="increment">button</button>
-  <h2>Students: {{ info }}</h2>
-  <h2>Instructor: {{ instructor.name }}</h2>
-  <h2>Instructor Bio: {{ instructor.bio }}</h2>
-  <h2>CourseInfo Sections: {{ courseInfo.sections.value }}</h2>
+  <p v-for="n in 10" :key="n">{{ n }}</p>
 </template>
-
