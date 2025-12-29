1e568b7d7f1cbc1f0c57ea91762c7d1110aa1b46
kimu805 <kimukimuok805@gmail.com>
2025-12-29 09:36:05 +0900

    refとreactive

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-  import { ref } from "vue"
+  import { ref, reactive } from "vue"
   const title = ref("Vue.js Course")
   let price = ref(9.99)
 
   function increment() {
     price.value += 1
+    instructor.bio = "hi"
   }
 
   const info = ref({
@@ -12,7 +13,24 @@
     rating: 4
   })
 
+  const instructor = reactive({
+    name: "Kazuto",
+    age: 25,
+    sns: {
+      twitter: "@kazuto",
+      youtube: "@kazuto_channel"
+    },
+    email: ref("kimukimuok805@gmail.com")
+  })
+
+  instructor.bio = "hello"
+  console.log(instructor.name)
   console.log(info.value.students)
+
+  const courseInfo = {
+    sections: ref(10),
+    language: ref("japanese")
+  }
 </script>
 
 <template>
@@ -20,5 +38,8 @@
   <h2>Price: ${{ price - 1 }}</h2>
   <button @click="increment">button</button>
   <h2>Students: {{ info }}</h2>
+  <h2>Instructor: {{ instructor.name }}</h2>
+  <h2>Instructor Bio: {{ instructor.bio }}</h2>
+  <h2>CourseInfo Sections: {{ courseInfo.sections.value }}</h2>
 </template>
 
