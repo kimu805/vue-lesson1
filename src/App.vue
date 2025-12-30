e58f9750f406d353db4ef12ca67459e5960d8b48
kimu805 <kimukimuok805@gmail.com>
2025-12-30 13:55:59 +0900

    inner

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { onMounted, useTemplateRef } from 'vue';
+
+const userInput = useTemplateRef("user-input")
+console.log(userInput.value)
+onMounted(() => {
+  userInput.value.focus()
+})
+</script>
+
+<template>
+  <input type="text" ref="user-input">
+</template>
