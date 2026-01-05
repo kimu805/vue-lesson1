369a5420dd22582fcf6926cc7096c98cfc325b5a
kimu805 <kimukimuok805@gmail.com>
2026-01-05 10:01:12 +0900

    slot

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <script setup>
-import { onMounted, useTemplateRef } from 'vue';
+import BaseCard from './components/BaseCard.vue';
 
-const userInput = useTemplateRef("user-input")
-console.log(userInput.value)
-onMounted(() => {
-  userInput.value.focus()
-})
 </script>
 
 <template>
-  <input type="text" ref="user-input">
+  <h1>Slots</h1>
+  <BaseCard>
+    <template #header="{ pageCount }">
+      <p>{{ pageCount }}</p>
+      <h2 v-if="pageCount === 1">Vue.js Course</h2>
+      <h2 v-if="pageCount === 2">HTML Course</h2>
+      <h2 v-if="pageCount === 3">CSS Course</h2>
+    </template>
+    <template #main="{ pageCount }">
+      <h2>this is a main</h2>
+      <p v-if="pageCount === 1">hello</p>
+      <p v-if="pageCount === 2">how are you?</p>
+      <p v-if="pageCount === 3">goodbye</p>
+    </template>
+    <template #footer>
+      <h2>this is a footer</h2>
+    </template>
+
+  </BaseCard>
 </template>
