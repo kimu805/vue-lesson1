7de75ce1209ee08b444dfa80afb3602bdfe62030
kimu805 <kimukimuok805@gmail.com>
2026-01-05 15:50:32 +0900

    composable

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <script setup>
+  import { ref } from 'vue';
+  import { useRefLimitedHistory } from './composables/refLimitedHistory';
+
+  const count = ref(0)
+
+  const { history, undo  } = useRefLimitedHistory(count, 3)
+  const userInput = ref("")
+  const { history: history2, undo: undo2 } = useRefLimitedHistory(userInput, 6)
 
 </script>
 
 <template>
-  
+  <h1>history</h1>
+  <p>count: {{ count }}</p>
+  <button @click="count++">increment</button>
+  <p>history: {{ history }}</p>
+  <button @click="undo">undo</button>
+
+  <h1>history2</h1>
+  <p>userInput: {{ userInput }}</p>
+  <input v-model="userInput" type="text">
+  <p>history2: {{ history2 }}</p>
+  <button @click="undo2">undo</button>
 </template>
